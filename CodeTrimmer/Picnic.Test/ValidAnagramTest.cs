46824ed4cda02080ed4cb0fa7da9bc030513113a
botocomo <botocomofr@gmail.com>
2016-06-08 14:34:35 -0700

    Clean up

--- CodeTrimmer/Picnic.Test/ValidAnagramTest.cs
+++ CodeTrimmer/Picnic.Test/ValidAnagramTest.cs
@@ -4,11 +4,26 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Picnic.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class SanityTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidAnagramTest()
         {
+            var source = "anagram";
+            var target = "nagaram";
+            var validAnagram = new ValidAnagram();
+            var result = validAnagram.IsAnagram(source, target);
+            Assert.IsTrue(result,
+                string.Format("Failed to perform pos test source: {0} target {1}", source, target));
+
+
+            source = "rat";
+            target = "car";
+            validAnagram = new ValidAnagram();
+            result = validAnagram.IsAnagram(source, target);
+            Assert.IsFalse(result,
+                string.Format("Failed to perform neg test source: {0} target {1}", source, target));
+
         }
     }
 }
--- CodeTrimmer/Picnic/ValidAnagram.cs
+++ CodeTrimmer/Picnic/ValidAnagram.cs
@@ -8,5 +8,47 @@ namespace Picnic
 {
     public class ValidAnagram
     {
+        public ValidAnagram() { }
+        public bool IsAnagram(string source, string target)
+        {
+          
+            if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(target) 
+                    || source.Length != target.Length)
+                return false;
+            Dictionary<char, int> sourceByCharMap = new Dictionary<char, int>();
+            for(int i=0; i < source.Length; i++)
+            {
+                var key = source.Substring(i, 1).ToLower().ToCharArray()[0];
+                if (key == ' ')
+                    continue;
+                if (sourceByCharMap.Keys.Contains(key))
+                {
+                    sourceByCharMap[key]++;
+
+                }
+                else
+                {
+                    sourceByCharMap.Add(key, 1);
+
+                }
+            }
+            for(int i=0; i < target.Length; i++)
+            {
+                var key = target.Substring(i, 1).ToLower().ToCharArray()[0];
+                if (key == ' ')
+                    continue;
+                if (sourceByCharMap.Keys.Contains(key) && sourceByCharMap[key] > 0)
+                {
+                    sourceByCharMap[key]--;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            return !sourceByCharMap.Where(x=>x.Value>0).Any();
+        }
     }
 }
