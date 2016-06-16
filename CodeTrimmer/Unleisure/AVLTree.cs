c2d9d6bf389ab8423a7e8e580130433960646ed0
botocomo <botocomofr@gmail.com>
2016-06-16 16:18:30 -0700

    a

--- CodeTrimmer/Unleisure/AVLTree.cs
+++ CodeTrimmer/Unleisure/AVLTree.cs
@@ -41,37 +41,124 @@ namespace Unleisure
                 queue.Enqueue(root);
                 while (queue.Count > 0)
                 {
-                    TreeNode tmp = queue.Dequeue();
-                    if (tmp.Value < target && tmp.Right!=null)
+                    TreeNode gp = queue.Dequeue();
+                    TreeNode rightParent = gp.Right;
+                    TreeNode leftParent = gp.Left;
+                    if (rightParent==null && leftParent == null)
                     {
-                        queue.Enqueue(tmp.Right);
-
-                    }else if (tmp.Value < target && tmp.Right == null)
-                    {
-                        tmp.Right = newNode;
+                        if (gp.Value< target)
+                        {
+                            gp.Right = newNode;
+                        }
+                        else
+                        {
+                            gp.Left = newNode;
+                        }
                     }
-                    else if (tmp.Value >= target && tmp.Left != null)
+                    else if (rightParent == null)
                     {
-                        queue.Enqueue(tmp.Left);
 
+                        if (gp.Value < target)
+                        {
+                            gp.Right = newNode;
+                          
+                        }
+                        else
+                        {
+                            queue.Enqueue(gp.Left);
+                        }
+
+                    }else if (leftParent == null)
+                    {
+
+                        if (gp.Value < target)
+                        {
+                            queue.Enqueue(gp.Right);
+                        }
+                        else
+                        {
+                            gp.Left = newNode;
+                          
+                        }
                     }
                     else
                     {
-                        tmp.Left = newNode;
+                        // Go to right and then right
+                        if (target > gp.Value && target > rightParent.Value )
+                        {
+                            if (rightParent.Right == null)
+                            {
+                                rightParent.Right = newNode;
+                                LeftRotate(gp);
+                            }
+                            else
+                            {
+
+                                queue.Enqueue(rightParent);
+                            }
+                        }
+                        // Go left and then left
+                        else if (target <= gp.Value && target <= leftParent.Value && leftParent.Left == null)
+                        {
+                            if (leftParent.Left == null) { 
+                                leftParent.Left = newNode;
+                                RightRotate(gp);
+                            }else
+                            {
+
+                                queue.Enqueue(rightParent);
+                            }
+                        }
+                        // Go Right and then left
+                        else if (target > gp.Value && target <= rightParent.Value )
+                        {
+                            if (rightParent.Left == null)
+                            {
+                                rightParent.Left = newNode;
+                                RightRotate(rightParent);
+                                LeftRotate(gp);
+                            }
+                            else
+                            {
+                                queue.Enqueue(rightParent);
+
+                            }
+                        }
+                        // Go to left and then right
+                        else if (target <= gp.Value && target > leftParent.Value)
+                        {
+                            if (leftParent.Right == null)
+                            {
+                                rightParent.Left = newNode;
+                                LeftRotate(rightParent);
+                                RightRotate(gp);
+                            }
+                            else
+                            {
+                                queue.Enqueue(leftParent);
+                            }
+                        }
+
+
                     }
+
+
                 }
             } 
         }
 
         protected void Adjust(TreeNode startNode)
         {
+            if (startNode == null)
+                return;
+
             if (Height(startNode.Left) - Height(startNode.Right) > 1)
             {
-
+                RightRotate(startNode);
 
             }else if (Height(startNode.Right) - Height(startNode.Left) > 1)
             {
-
+                LeftRotate(startNode);
             }else{
                 Adjust(startNode.Left);
                 Adjust(startNode.Right);
