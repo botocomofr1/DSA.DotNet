adba828e2aacf0fe4077c5d47705f3c68853bbcb
botocomo <botocomofr@gmail.com>
2016-06-16 15:25:02 -0700

    a

---
+++ CodeTrimmer/Picnic/LinkList.cs
@@ -0,0 +1,67 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picnic
+{
+    public class MyLinkList
+    {
+        private LinkNode root = null;
+        public MyLinkList() { }
+
+        public void Add(LinkNode node)
+        {
+            if (root == null)
+                root = node;
+            else
+            {
+                LinkNode tmp = root;
+                while (tmp.Next != null)
+                    tmp = tmp.Next;
+                tmp.Next = node;
+            }
+
+        }
+        public LinkNode Remove(LinkNode node)
+        {
+            if (node == null)
+                return null;
+            else
+            {
+                LinkNode current = root;
+                while (current != null && current.Value != node.Value)
+                {
+                    current = current.Next;
+                }
+                if (current == null)
+                    return null;
+                else {
+                    var deleted = current.Next;
+                    var nextNext = current.Next.Next;
+                    current.Next = nextNext;
+                    deleted.Next = null;
+                    return deleted;
+                }
+            }
+
+        }
+
+        public void Reverse()
+        {
+
+            LinkNode newNode = null;
+            LinkNode current = root;
+            while (current != null)
+            {
+                LinkNode temp = current;
+                temp.Next = newNode;
+                newNode = temp;
+                current = current.Next;
+            }
+
+        }
+    }
+}
---
+++ CodeTrimmer/Unleisure/AVLTree.cs
@@ -0,0 +1,99 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unleisure
+{
+    public class AVLTree
+    {
+        private TreeNode  root=null;
+        public AVLTree() { }
+
+        protected int Height(TreeNode startNode)
+        {
+            if (startNode == null)
+                return 0;
+            else
+            {
+                int lheight = Height(startNode.Left);
+                int rheight = Height(startNode.Right);
+                return lheight > rheight ? lheight : rheight;
+            }
+
+        }
+
+        public void Insert(int target)
+        {
+            var newNode = new TreeNode();
+            newNode.Value = target;
+            if (root == null)
+            {
+            
+                root = newNode;
+
+            }
+            else
+            {
+                Queue<TreeNode> queue = new Queue<TreeNode>();
+                queue.Enqueue(root);
+                while (queue.Count > 0)
+                {
+                    TreeNode tmp = queue.Dequeue();
+                    if (tmp.Value < target && tmp.Right!=null)
+                    {
+                        queue.Enqueue(tmp.Right);
+
+                    }else if (tmp.Value < target && tmp.Right == null)
+                    {
+                        tmp.Right = newNode;
+                    }
+                    else if (tmp.Value >= target && tmp.Left != null)
+                    {
+                        queue.Enqueue(tmp.Left);
+
+                    }
+                    else
+                    {
+                        tmp.Left = newNode;
+                    }
+                }
+            } 
+        }
+
+        protected void Adjust(TreeNode startNode)
+        {
+            if (Height(startNode.Left) - Height(startNode.Right) > 1)
+            {
+
+
+            }else if (Height(startNode.Right) - Height(startNode.Left) > 1)
+            {
+
+            }else{
+                Adjust(startNode.Left);
+                Adjust(startNode.Right);
+
+            }
+
+        }
+
+        protected void RightRotate(TreeNode startNode)
+        {
+            TreeNode newP = startNode.Left;
+            startNode.Left = newP.Right;
+            newP.Right = startNode;
+
+        }
+
+        protected void LeftRotate(TreeNode startNode)
+        {
+            TreeNode newP = startNode.Right;
+            startNode.Right = newP.Left;
+            newP.Right = startNode;
+
+        }
+    }
+}
