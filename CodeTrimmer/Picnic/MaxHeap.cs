b191790d394f551d5dc55b45cbd859c08ead7223
botocomo <botocomofr@gmail.com>
2016-06-16 11:23:07 -0700

    a

---
+++ CodeTrimmer/Picnic/MaxHeap.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picnic
+{
+    public class MaxHeap
+    {
+        private List<int> heapValueList = new List<int>();
+
+        public MaxHeap()
+        {
+
+            heapValueList.Insert(0, int.MaxValue);
+        }
+
+        public int GetMin()
+        {
+            if (heapValueList.Count >= 1)
+            {
+                int value = heapValueList[1];
+
+                Delete(value);
+                Heapify();
+                return value;
+            }
+            else
+                throw new Exception("Empty Heap");
+        }
+
+        public void Delete(int value)
+        {
+            if (heapValueList.Count >= 1)
+            {
+                int index = heapValueList.IndexOf(value);
+                int last = heapValueList.Count - 1;
+                heapValueList[index] = heapValueList[last];
+                heapValueList.Remove(last);
+                Heapify();
+            }
+            else
+            {
+                throw new Exception("Empty Heap");
+            }
+        }
+
+        public void Insert(int value)
+        {
+            heapValueList.Add(value);
+            Heapify();
+        }
+
+        // Start from bottom and move largest upward
+        protected void Heapify()
+        {
+            int parentIndex = heapValueList.Count / 2;
+            int currentIndex = heapValueList.Count - 1;
+            while (parentIndex >= 1)
+            {
+                if (heapValueList[currentIndex] > heapValueList[parentIndex])
+                {
+                    int tmp = heapValueList[parentIndex];
+                    heapValueList[parentIndex] = heapValueList[currentIndex];
+                    heapValueList[currentIndex] = tmp;
+                }
+                currentIndex = parentIndex;
+                parentIndex = currentIndex;
+            }
+
+        }
+
+    }
+}
---
+++ CodeTrimmer/Picnic/MinHeap.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picnic
+{
+    public class MinHeap
+    {
+        private List<int> heapValueList = new List<int>();
+
+        public MinHeap()
+        {
+
+            heapValueList.Insert(0, int.MaxValue);
+        }
+
+        public int GetMin()
+        {
+            if (heapValueList.Count >= 1)
+            {
+                int value = heapValueList[1];
+
+                Delete(value);
+                Heapify();
+                return value;
+            }
+            else
+                throw new Exception("Empty Heap");
+        }
+
+        public void Delete(int value)
+        {
+            if (heapValueList.Count >= 1)
+            {
+                int index = heapValueList.IndexOf(value);
+                int last = heapValueList.Count - 1;
+                heapValueList[index] = heapValueList[last];
+                heapValueList.Remove(last);
+                Heapify();
+            }
+            else
+            {
+                throw new Exception("Empty Heap");
+            }
+        }
+
+        public void Insert(int value)
+        {
+            heapValueList.Add(value);
+            Heapify();
+        }
+        // Start from bottom and move the smallest upward
+        protected void Heapify()
+        {
+            int parentIndex = heapValueList.Count / 2;
+            int childIndex = heapValueList.Count - 1;
+            while (parentIndex >= 1)
+            {
+                if (heapValueList[childIndex] < heapValueList[parentIndex])
+                {
+                    int tmp = heapValueList[parentIndex];
+                    heapValueList[parentIndex] = heapValueList[childIndex];
+                    heapValueList[childIndex] = tmp;
+                }
+                childIndex = parentIndex;
+                parentIndex = childIndex;
+            }
+
+        }
+
+    }
+}
