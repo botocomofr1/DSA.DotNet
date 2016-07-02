aaebb69bac97d9e5489fd5b8c94af16953091b2c
botocomofr1 <botocomofr1@gmail.com>
2016-07-01 23:52:52 -0700

    Remove to array

--- CodeTrimmer/Picnic.Test/RemoveDuplicateArrayTest.cs
+++ CodeTrimmer/Picnic.Test/RemoveDuplicateArrayTest.cs
@@ -14,13 +14,13 @@ namespace Picnic.Test
          
             int[] array = new int[] { 1, 1, 1 };
             int i = removeDupInt.Remove(ref array);
-            Console.WriteLine(string.Join(",",array.Take(i+1).ToArray()));
+            Console.WriteLine(string.Join(",",array.Take(i+1)));
             array = new int[] { 1, 1, 1,2 };
             i = removeDupInt.Remove(ref array);
-            Console.WriteLine(string.Join(",", array.Take(i + 1).ToArray()));
+            Console.WriteLine(string.Join(",", array.Take(i + 1)));
             array = new int[] { 1, 1, 1, 2,2,19 };
             i = removeDupInt.Remove(ref array);
-            Console.WriteLine(string.Join(",", array.Take(i + 1).ToArray()));
+            Console.WriteLine(string.Join(",", array.Take(i + 1)));
         }
     }
 }
