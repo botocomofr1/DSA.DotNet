176266bac716403426477c65a6d85b275b098619
botocomofr1 <botocomofr1@gmail.com>
2016-07-01 23:51:43 -0700

    Added Remove Duplicate Int

---
+++ CodeTrimmer/Picnic.Test/RemoveDuplicateArrayTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+namespace Picnic.Test
+{
+    [TestClass]
+    public class RemoveDuplicateArrayTest
+    {
+        [TestMethod]
+        public void RemoveDupIntTest()
+        {
+            var removeDupInt = new RemoveDuplicateArray();
+         
+            int[] array = new int[] { 1, 1, 1 };
+            int i = removeDupInt.Remove(ref array);
+            Console.WriteLine(string.Join(",",array.Take(i+1).ToArray()));
+            array = new int[] { 1, 1, 1,2 };
+            i = removeDupInt.Remove(ref array);
+            Console.WriteLine(string.Join(",", array.Take(i + 1).ToArray()));
+            array = new int[] { 1, 1, 1, 2,2,19 };
+            i = removeDupInt.Remove(ref array);
+            Console.WriteLine(string.Join(",", array.Take(i + 1).ToArray()));
+        }
+    }
+}
---
+++ CodeTrimmer/Picnic/RemoveDuplicateArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picnic
+{
+    public class RemoveDuplicateArray
+    {
+        public RemoveDuplicateArray() { }
+
+        public int Remove(ref int[] array)
+        {
+            List<int> result = new List<int>();
+            int i = 0;
+            int j = 1;
+            if (array.Length > 1)
+            {
+                for (; j < array.Length; j++)
+                {
+                    if (array[i] != array[j])
+                    {
+                        array[i + 1] = array[j];
+                        i++;
+
+                    }
+                }
+
+            }
+       
+            return i;
+        }
+    }
+}
