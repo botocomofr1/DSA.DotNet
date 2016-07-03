27decc53bcc358468a2641b01d4a1a5159fef457
botocomofr1 <botocomofr1@gmail.com>
2016-07-03 08:55:11 -0700

    Added PowerSet

---
+++ CodeTrimmer/Picnic.Test/PowerSetTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Picnic.Test
+{
+    [TestClass]
+    public class PowerSetTest
+    {
+        [TestMethod]
+        public void PowerSetOfEltTest()
+        {
+            var powerSet = new PowerSetofElement();
+            List<int> elts = new List<int>();
+            elts.Add(1);
+            elts.Add(2);
+            elts.Add(3);
+            powerSet.PrintThePowerSet(elts);
+
+        }
+    }
+}
---
+++ CodeTrimmer/Picnic/PowerSetofElement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picnic
+{
+    public class PowerSetofElement
+    {
+        public PowerSetofElement() { }
+
+        public void PrintThePowerSet(List<int> elt)
+        {
+            int len = elt.Count;
+            if (len <= 0)
+                Console.WriteLine("Empty");
+            var combinations = Convert.ToInt32(Math.Pow(2, len));
+
+            for(int i = 1; i <= combinations; i++)
+            {
+                List<int> thisCombination = new List<int>();
+                int eltPos = 1;
+                for(int count = 0;count< elt.Count; count++)
+                {
+                    if ((eltPos & i) > 0)
+                    {
+                        thisCombination.Add(elt[count]);
+                    }
+                    eltPos = eltPos << 1;
+                }
+                Console.WriteLine(string.Join(",", thisCombination));
+
+            }
+
+
+        }
+    }
+}
