111bab54cabd76083c0e4beef3f272ceb6b3c284
botocomofr1 <botocomofr1@gmail.com>
2016-07-04 20:19:45 -0700

    added ip address range

---
+++ CodeTrimmer/Unleisure/IpAddressRange.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Linq;
+namespace Unleisure
+{
+    public class IpAddressRange
+    {
+        IDictionary<int, string> sortedIP = new Dictionary<int, string>();
+        public IpAddressRange() { }
+
+        public void AddIp(string ip)
+        {
+            if (!IsVaid(ip))
+                throw new Exception("Not valid IP");
+            string[] ip4parts = ip.Split('.');
+
+            int ipInInteger = 0;
+            for (int i = 0; i < ip4parts.Length; i++)
+            {
+                int eachPartInInteger = Convert.ToInt32(ip4parts[i]);
+                if (eachPartInInteger > 255 || eachPartInInteger < 0)
+                    throw new Exception("Not Valid IP");
+                int shiftBits = ((ip4parts.Length - 1) - i) * 8;
+                eachPartInInteger = eachPartInInteger << shiftBits;
+                ipInInteger += eachPartInInteger;
+            }
+            sortedIP.Add(ipInInteger, ip);
+
+
+        }
+
+        public List<string> GetIpRange(int min , int max)
+        {
+            var ipRange = sortedIP.Where(x => x.Key >= min && x.Key <=max).Select(y=>y.Value);
+            return ipRange.ToList<string>();
+
+        }
+        public bool IsVaid(string ip)
+        {
+            if (string.IsNullOrEmpty(ip))
+                return false;
+            string ipAddressP = @"\b(?:\d{1,3}\.){3}\d{1,3}\b";
+            Regex regExp = new Regex(ipAddressP);
+            return regExp.IsMatch(ip);
+
+        }
+    }
+}
---
+++ CodeTrimmer/UnlesisureUnitTest/IpRanageTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unleisure;
+
+namespace UnlesisureUnitTest
+{
+    [TestClass]
+    public class IpRanageTest
+    {
+        [TestMethod]
+        public void SimpleIPRangeTest()
+        {
+            var ipRange = new IpAddressRange();
+            Console.WriteLine(ipRange.IsVaid("255.255.225.222"));
+            Console.WriteLine(ipRange.IsVaid("1.1.1.1"));
+            ipRange.AddIp("0.0.0.1");
+            ipRange.AddIp("0.0.1.0");
+            ipRange.AddIp("0.1.0.0");
+            ipRange.AddIp("1.0.0.0");
+            Console.WriteLine(string.Join(",",
+            ipRange.GetIpRange(1, Convert.ToInt32(Math.Pow(2, 24)) + 1)));
+
+
+
+        }
+    }
+}
