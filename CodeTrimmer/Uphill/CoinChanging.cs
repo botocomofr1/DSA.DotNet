1c1fc635ed7ab3f2d65e65ad731264031ffa4429
botocomofr1 <botocomofr1@gmail.com>
2016-07-13 22:24:31 -0700

    correct

--- CodeTrimmer/Uphill/CoinChanging.cs
+++ CodeTrimmer/Uphill/CoinChanging.cs
@@ -10,15 +10,15 @@ namespace UphillUnitTest
     {
         private Dictionary<int, int> minCentsCoinNumber = new Dictionary<int, int>();
 
-        private Dictionary<Tuple<int, int>, int> lastUsedCoin = new Dictionary<Tuple<int, int>, int>();
+        private Dictionary<int ,int> lastUsedCoin = new Dictionary<int, int>();
         private List<int> possibleCoins = new List<int>();
         public CoinChanging()
         {
             minCentsCoinNumber = new Dictionary<int, int>();
             minCentsCoinNumber.Add(0, 0);
 
-            lastUsedCoin = new Dictionary<Tuple<int, int>, int>();
-            lastUsedCoin.Add(new Tuple<int, int>(0, 0), 0);
+            lastUsedCoin = new Dictionary<int, int>();
+            lastUsedCoin.Add( 0, 0);
         }
 
         public int MinNumberOfCoins(int cents, List<int> possibleCoins)
@@ -29,10 +29,11 @@ namespace UphillUnitTest
 
             this.possibleCoins = possibleCoins;
             int result = Calculate(cents);
-            //foreach (var key in minCentsCoinNumber1.Keys)
-            //{
-            //    Console.WriteLine(string.Format("{0} Cents, [{1} Coin]  : {2}", key.Item1, key.Item2, minCentsCoinNumber1[key]));
-            //}
+   
+            foreach(var key in lastUsedCoin.Keys)
+            {
+                Console.WriteLine(string.Format("Cent {0} Coin {1}", key, lastUsedCoin[key]));
+            }
             return result;
         }
 
@@ -45,28 +46,19 @@ namespace UphillUnitTest
             int minCoins = Int32.MaxValue;
             foreach (var coinValue in possibleCoins)
             {
-                var key = new Tuple<int, int>(cents, coinValue);
-                if (lastUsedCoin.ContainsKey(key))
-                {
 
-                    int tmp = lastUsedCoin[key];
-                    minCoins = Calculate(cents - coinValue) + tmp;
-                    lastUsedCoin[key] = minCoins;
-                    
-
-                }
-                else if (cents >= coinValue)
+                if (cents >= coinValue)
                 {
                     int tmp = Calculate(cents - coinValue) + 1;
+                   
+                    if (tmp < minCoins)
+                        lastUsedCoin[cents] = coinValue;
                     minCoins = Math.Min(minCoins, tmp);
-                    lastUsedCoin[key] = minCoins;
+                    
+                    
+                }
 
-                }
-                else
-                {
-                    lastUsedCoin[key] = 0;
-                }
-                
+
             }
 
             minCentsCoinNumber[cents] = minCoins;
