54dcc00f325282b76039d8ae39b3f53628718262
botocomofr1 <botocomofr1@gmail.com>
2016-07-18 21:34:11 -0700

    work in progress

---
+++ CodeTrimmer/Uphill/WordSplit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uphill
+{
+    public class WordSplit
+    {
+        private int index;
+        private HashSet<string> wordsMap = new HashSet<string>();
+        public WordSplit()
+        {
+      
+
+
+        }
+
+        public bool IsSplitable(List<char> splitableword, HashSet<string> wordsMap)
+        {
+            this.wordsMap = wordsMap;
+            if (splitableword.Count <= 0)
+                return false;
+
+     
+            return IsSplitable(0, splitableword.Count - 1, splitableword);
+
+        }
+
+        protected bool IsSplitable(int i, int j, List<char> splitableWord)
+        {
+            if (i < 0  || i>j || j > splitableWord.Count - 1)
+                return false;
+         
+            StringBuilder sb = new StringBuilder();
+      
+            for (int index = i; index <= j; index++)
+            {
+                sb.Append(splitableWord[index]);
+            }
+         
+            var isSplitable = false;
+     
+            if (wordsMap.Contains(sb.ToString().ToUpper())) 
+                return true;
+            else
+            {
+                for (int k = i; k < j; k++)
+                {
+                    
+                    isSplitable= IsSplitable(i, k, splitableWord) && IsSplitable(k + 1, j, splitableWord);
+                }
+            }
+          
+            if (isSplitable)
+                wordsMap.Add(sb.ToString().ToUpper());
+            return isSplitable;
+        }
+    }
+}
---
+++ CodeTrimmer/UphillUnitTest/WordSplitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uphill;
+using System.Collections.Generic;
+using System.Linq;
+namespace UphillUnitTest
+{
+    [TestClass]
+    public class WordSplitTest
+    {
+        [TestMethod]
+        public void WordSplitSimpleTest()
+        {
+        
+
+            HashSet<string> words = new HashSet<string>();
+            words.Add("A");
+            words.Add("AM");
+            words.Add("ACE");
+            words.Add("I");
+            var wordSplit = new WordSplit();
+            Console.WriteLine(wordSplit.IsSplitable("IAMACE".ToCharArray().ToList(), words));
+
+
+        }
+    }
+}
